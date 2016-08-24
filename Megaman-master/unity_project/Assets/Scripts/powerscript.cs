0b44872aad58379e9fe92be5fd964579b6756453
yuvaGS1 <ge.yuvaraja@gmail.com>
2016-08-24 14:41:01 +0530

    keyboard keys activated in powerpack

--- Megaman-master/unity_project/Assets/Scripts/powerscript.cs
+++ Megaman-master/unity_project/Assets/Scripts/powerscript.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class powerscript : MonoBehaviour {
 	public Text theText;
+	public GameObject power;
 
 	// Use this for initialization
 	void Start () {
@@ -12,8 +13,12 @@ public class powerscript : MonoBehaviour {
 	}
 	// Update is called once per frame
 	void Update () {
-	
 	} 
+
+	void FixedUpdate () 
+	{
+		PowerPacks();
+	}
 
 	public void buttonpress(int num)
 	{
@@ -30,4 +35,66 @@ public class powerscript : MonoBehaviour {
 		yield return new WaitForSeconds (1);
 		Application.LoadLevel(level);
 	}
+	private void PowerPacks()
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha0)) 
+		{
+			theText.text = "0";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+			theText.text = "1";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) 
+		{
+			theText.text = "2";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) 
+		{
+			theText.text = "3";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4)) 
+		{
+			theText.text = "4";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha5)) 
+		{
+			theText.text = "5";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha6)) 
+		{
+			theText.text = "6";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha7)) 
+		{
+			theText.text = "7";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha8)) 
+		{
+			theText.text = "8";
+			StartCoroutine(PowerLayers ());
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha9)) 
+		{
+			theText.text = "9";
+			StartCoroutine(PowerLayers ());
+		}
+	}
+	IEnumerator PowerLayers()
+	{
+		yield return new WaitForSeconds(5);
+		GameObject.Find ("powerpack").transform.localScale = new Vector3(0, 0, 0);
+	}
+	
+	
+
+
 }
